8cb4f09e598ca43ee9c4f1ec6d86d930e2922ecb
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-04-04 18:35:38 -0700

    Created TCP Connecting with Parse Error Enum

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,71 @@
 
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult, Display, Debug, Formatter, write};
+use std::str::{self, Utf8Error};
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method, //super tells rust to go up one level to find the method module
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!()
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        //    match str::from_utf8(buf) {
+        //     Ok(request) => {},
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        //    }
+       let request = str::from_utf8(buf)?;
+    }
+ 
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidMethod => "InvalidMethod",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        ParseError::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {
+
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
-
+use std::{net::TcpListener, io::Read};
+use crate::http::{Request, request};
+use std::convert::TryFrom;
 pub struct Server {
     addr: String,
 }
@@ -11,6 +13,33 @@ impl Server {
 
     //methods always accept self as first parameter
     pub fn run(self) {
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
+
+        //creating the socket
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        //listening to accept connections
+        loop {
+
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a Request: {}", String::from_utf8_lossy(&buffer));
+
+                            //need to explicity
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to Parse Request: {}", e),
+                            }
+                        },
+                        Err(e) => println!("{}", e),
+                    }
+                },
+                Err(e) => println!("{}", e),
+                
+            }
+        }
     }
 }
